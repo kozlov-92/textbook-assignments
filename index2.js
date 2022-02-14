241493a719328f02ba757bab20f2699baba2e442
kozlov-92 <kozlov.an92@yandex.ru>
2022-02-14 08:26:06 +0300

    made changes in master

---
+++ index2.js
@@ -0,0 +1,14 @@
+// const myFriend = ['Andrey', 'Sergey', 'Semen', 'Alex', 'Korobyan']
+// $('#main-heading').text('My friend')
+// for (let i = 0; i < myFriend.length; i++) {
+//   $('p').hide(5000)
+//   $('body').append('<p> - ' + myFriend[i] + '</p>')
+// }
+// $('p').append('- best')
+
+const userAge = 35
+if (userAge >= 18) {
+  console.log('Вы совершеннолетний')
+} else {
+  console.log('Вы НЕ совершеннолетний')
+}
