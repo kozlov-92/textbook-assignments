f3a40acfbfb6d17edd080c58862adfb2f1018c51
kozlov-92 <kozlov.an92@yandex.ru>
2022-02-14 08:20:31 +0300

    made changes in feature/user-access

---
+++ index2.js
@@ -0,0 +1,13 @@
+// const myFriend = ['Andrey', 'Sergey', 'Semen', 'Alex', 'Korobyan']
+// $('#main-heading').text('My friend')
+// for (let i = 0; i < myFriend.length; i++) {
+//   $('p').hide(5000)
+//   $('body').append('<p> - ' + myFriend[i] + '</p>')
+// }
+// $('p').append('- best')
+const age = 18
+if (age >= 18) {
+  console.log('Вы совершеннолетний')
+} else {
+  console.log('Вы НЕ совершеннолетний')
+}
